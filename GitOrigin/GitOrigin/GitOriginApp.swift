80886f7e8ef5b5b42d60af34708611c8341ecaad
Divi Newton <divinewton@gmail.com>
2026-06-14 15:19:52 -0700

    Xcode project setup
    Initial project scaffolding for the GitOrigin macOS app: adds Xcode project and workspace files, asset catalogs (AppIcon, AccentColor), and basic SwiftUI app entry (GitOriginApp.swift) with a ContentView. Project settings include Swift 5.0, macOS deployment target 26.0, and product bundle identifier divinewton.GitOrigin.

---
+++ GitOrigin/GitOrigin/GitOriginApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitOriginApp.swift
+//  GitOrigin
+//
+//  Created by Divi Newton on 6/14/26.
+//
+
+import SwiftUI
+
+@main
+struct GitOriginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
